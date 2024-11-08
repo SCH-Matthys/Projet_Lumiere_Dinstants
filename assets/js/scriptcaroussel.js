0d1f9508ccb77206c055639bff49c95326eda629
Gipsy Rouaret <gipsy.rouaret@orange.fr>
2024-11-08 16:41:16 +0100

    responsivité4

---
+++ assets/js/scriptcaroussel.js
@@ -0,0 +1,17 @@
+const greetingContainer=document.querySelector(".greeting_container");
+const galleryControlContainer=document.querySelector(".gallery_control");
+const rightbtnone=document.querySelector("#rightbtnone");
+const rightbtntwo=document.querySelector("#rightbtntwo");
+const rightbtnthree=document.querySelector("#rightbtnthree");
+const leftbtnone=document.querySelector("#leftbtnone");
+const leftbtntwo=document.querySelector("#leftbtntwo");
+const leftbtnthree=document.querySelector("#leftbtnthree");
+const pict1=document.querySelector(".pict_1");
+const pict2=document.querySelector(".pict_2");
+const pict3=document.querySelector(".pict_3");
+const images =[pict1,pict2,pict3];
+let index=0;
+
+rightbtnone.addEventListener("click",function(){
+      
+})
